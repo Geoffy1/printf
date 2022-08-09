3536e31ab8cc2f2b256effbe7c56b0f5ebf2df42
Geoffy1 <geojogu@gmail.com>
2022-08-08 17:06:47 -0700

    prints power indices

--- print_base.c
+++ print_base.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
+ * Description: the function calls convert()
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
