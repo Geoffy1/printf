e142f59ba36fd5b6f47a7ff2a3109538b89ee0a3
Geoffy1 <geojogu@gmail.com>
2022-08-08 17:03:51 -0700

    writes putchar fns

--- writes.c
+++ writes.c
@@ -1,10 +1,10 @@
-nclude "main.h"
+#include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ * On error, -1 is returned, and errno is set
  * Description: _putchar uses a local buffer of 1024 to 
  * as little as possible
  */
